fe856e8d936adaa2592afc8a9611a188ea09b454
Francisco Javier Jimenez <>
2025-02-13 07:42:18 +0100

    Add: ProveedorServiceTest

--- src/test/java/com/prueba_metrica/demo_proveedores/domain/ProveedorServiceTest.java
+++ src/test/java/com/prueba_metrica/demo_proveedores/domain/ProveedorServiceTest.java
@@ -7,11 +7,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,5 +42,24 @@ class ProveedorServiceTest {
 
         assertThat(result).hasSize(2);
         assertThat(result).extracting("nombre").containsExactlyInAnyOrder("Proveedor A", "Proveedor B");
+    }
+
+    @Test
+    void shouldCrerateProveedor(){
+        Proveedor proveedor3= new Proveedor(1l, "Pepsi", LocalDate.now(), 8L);
+        Proveedor savedProveedor = new Proveedor(1L, "Pepsi",LocalDate.now(), 8L );
+        when(proveedorRepository.save(any(Proveedor.class))).thenReturn(savedProveedor);
+
+        Proveedor result =  proveedorService.crearProveedor(proveedor3);
+
+        assertNotNull(result);
+        assertEquals(savedProveedor.getId(), result.getId());
+        assertEquals(savedProveedor.getNombre(), result.getNombre());
+        assertEquals(savedProveedor.getClientId(), result.getClientId());
+        verify(proveedorRepository).save(proveedor3);
+
+
+
+
     }
 }
