b064599ae1e0a374a560cb66ff39f3e1cb2d9a7a
Francisco Javier Jimenez <>
2025-02-21 10:10:32 +0100

    Add: repistory delete test

--- src/test/java/com/prueba_metrica/demo_proveedores/infrastructure/ProveedorRepositoryTest.java
+++ src/test/java/com/prueba_metrica/demo_proveedores/infrastructure/ProveedorRepositoryTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class ProveedorRepositoryTest {
@@ -23,5 +26,17 @@ class ProveedorRepositoryTest {
 
         // THEN
         assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void shouldDeleteProveedor(){
+        Proveedor proveedor1 = new Proveedor(1L, "Proveedor A", LocalDate.now(), 101L);
+        Proveedor savedProveedor = proveedorRepository.save(proveedor1);
+
+        proveedorRepository.deleteById(savedProveedor.getId());
+
+        Optional<Proveedor> found = proveedorRepository.findById(savedProveedor.getId());
+        assertFalse(found.isPresent());
+
     }
 }
