1b49883b9cff09878917898d04a36cce7781998a
fjjjt3 <fjjt3@yahoo.com>
2025-03-16 14:25:26 +0100

    Add: CORS

--- src/main/java/com/prueba_metrica/demo_proveedores/interfaces/ProveedorController.java
+++ src/main/java/com/prueba_metrica/demo_proveedores/interfaces/ProveedorController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/proveedores")
 public class ProveedorController {
 
